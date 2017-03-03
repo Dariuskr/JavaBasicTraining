b2841cf63b79181fae3016f6d0770a56ff212cc9
teinted321 <teintedteinted@yahoo.com>
2017-03-03 20:18:58 +0200

    Vardo įvedimas 5 kartus

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by Max on 2017.03.03.
  */
 public class Main {
+
+    public static void main (String[] args)
+    {   System.out.println("Įvesk savo vardą");
+        Scanner skaneris = new Scanner(System.in);
+        String vardas = skaneris.nextLine();
+
+        System.out.println("tavo vardas yra" + vardas);
+        System.out.println("tavo vardas yra" + vardas);
+        System.out.println("tavo vardas yra" + vardas);
+        System.out.println("tavo vardas yra" + vardas);
+        System.out.println("tavo vardas yra" + vardas);
+    }
 
 }
